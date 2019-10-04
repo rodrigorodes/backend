0bb1106e43dbe35bbe4b84c336cfa67c61702dee
Rodrigo Mendes <ro.mendes1989@gmail.com>
2019-10-04 11:25:37 +0200

    create project

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const sessionController = require('./controllers/SessionController');
+const spotController = require('./controllers/SpotController');
+const dashboardController = require('./controllers/DashboardController');
+const bookingController = require('./controllers/BookingController');
+
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+routes.post('/sessions', sessionController.store);
+
+routes.post('/spots', upload.single('thumbnail'), spotController.store);
+
+routes.get('/spots', spotController.index);
+
+routes.post('/spots/:spot_id/bookings', bookingController.store);
+
+routes.get('/dashboard', dashboardController.show);
+
+module.exports = routes;
