a4914c7f2fb98cd96db4b0bcdd64461aec971c69
Rodrigo Mendes <ro.mendes1989@gmail.com>
2019-10-04 13:16:29 +0200

    fix cors

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb+srv://rods:rods@oministack-jztub.mongodb.net/aircnc?re
 var port = process.env.PORT || 3333;
 
 
-app.use(cors());
+app.use(cors({ origin: 'https://airfrontend.herokuapp.com' }));
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
